39c631fc99b115967ef96b8edb77ce84b2f6b2ba
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-20 11:44:22 +0700

    model buku

---
+++ application/models/M_buku.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_buku extends CI_Model
+{
+
+	// List all your items
+	public function buku()
+	{
+		$this->db->select('*');
+		$this->db->from('buku');
+		$this->db->order_by('id_buku', 'desc');
+		return $this->db->get()->result();
+	}
+
+	// Add a new item
+	public function add($data)
+	{
+		$this->db->insert('buku', $data);
+	}
+
+	public function detail($id_buku)
+	{
+		$this->db->select('*');
+		$this->db->from('buku');
+		$this->db->where('id_buku', $id_buku);
+		return $this->db->get()->row();
+	}
+
+	//Update one item
+	public function update($data)
+	{
+		$this->db->where('id_buku', $data['id_buku']);
+		$this->db->update('buku', $data);
+	}
+
+	//Delete one item
+	public function delete($data)
+	{
+		$this->db->where('id_buku', $data['id_buku']);
+		$this->db->delete('buku', $data);
+	}
+}
+
+/* End of file M_buku.php */
