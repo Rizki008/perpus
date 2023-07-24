7845ef487028b82de9aa90b6f04c285bdbfb9c32
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-24 08:42:11 +0700

    menambahkan grafik baca pinjam dan favorit

--- application/views/backend/v_admin.php
+++ application/views/backend/v_admin.php
@@ -38,12 +38,12 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-12">
+			<div class="col-lg-6">
 				<div class="ibox">
 					<div class="ibox-body">
 						<div class="flexbox mb-4">
 							<div>
-								<h3 class="m-0">Grafik Buku Dipinjam</h3>
+								<h3 class="m-0">Grafik Buku Favorit</h3>
 							</div>
 						</div>
 						<div>
@@ -61,8 +61,248 @@
 									data: {
 										labels: <?= json_encode($judul) ?>,
 										datasets: [{
-											label: 'Grafik Penjualan',
+											label: 'Grafik Buku Favorit',
 											data: <?= json_encode($jml_buku) ?>,
+											backgroundColor: [
+												'rgba(255, 99, 132, 0.80)',
+												'rgba(54, 162, 235, 0.80)',
+												'rgba(255, 206, 86, 0.80)',
+												'rgba(75, 192, 192, 0.80)',
+												'rgba(153, 102, 255, 0.80)',
+												'rgba(255, 159, 64, 0.80)',
+												'rgba(201, 76, 76, 0.3)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(0, 140, 162, 1)',
+												'rgba(158, 109, 8, 1)',
+												'rgba(201, 76, 76, 0.8)',
+												'rgba(0, 129, 212, 1)',
+												'rgba(201, 77, 201, 1)',
+												'rgba(255, 207, 207, 1)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(128, 98, 98, 1)',
+												'rgba(0, 0, 0, 1)',
+												'rgba(128, 128, 128, 1)',
+												'rgba(255, 99, 132, 0.80)',
+												'rgba(54, 162, 235, 0.80)',
+												'rgba(255, 206, 86, 0.80)',
+												'rgba(75, 192, 192, 0.80)',
+												'rgba(153, 102, 255, 0.80)',
+												'rgba(255, 159, 64, 0.80)',
+												'rgba(201, 76, 76, 0.3)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(0, 140, 162, 1)',
+												'rgba(158, 109, 8, 1)',
+												'rgba(201, 76, 76, 0.8)',
+												'rgba(0, 129, 212, 1)',
+												'rgba(201, 77, 201, 1)',
+												'rgba(255, 207, 207, 1)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(128, 98, 98, 1)',
+												'rgba(0, 0, 0, 1)',
+												'rgba(128, 128, 128, 1)'
+											],
+											borderColor: [
+												'rgba(255, 99, 132, 1)',
+												'rgba(54, 162, 235, 1)',
+												'rgba(255, 206, 86, 1)',
+												'rgba(75, 192, 192, 1)',
+												'rgba(153, 102, 255, 1)',
+												'rgba(255, 159, 64, 1)',
+												'rgba(201, 76, 76, 0.3)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(0, 140, 162, 1)',
+												'rgba(158, 109, 8, 1)',
+												'rgba(201, 76, 76, 0.8)',
+												'rgba(0, 129, 212, 1)',
+												'rgba(201, 77, 201, 1)',
+												'rgba(255, 207, 207, 1)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(128, 98, 98, 1)',
+												'rgba(0, 0, 0, 1)',
+												'rgba(128, 128, 128, 1)',
+												'rgba(255, 99, 132, 1)',
+												'rgba(54, 162, 235, 1)',
+												'rgba(255, 206, 86, 1)',
+												'rgba(75, 192, 192, 1)',
+												'rgba(153, 102, 255, 1)',
+												'rgba(255, 159, 64, 1)',
+												'rgba(201, 76, 76, 0.3)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(0, 140, 162, 1)',
+												'rgba(158, 109, 8, 1)',
+												'rgba(201, 76, 76, 0.8)',
+												'rgba(0, 129, 212, 1)',
+												'rgba(201, 77, 201, 1)',
+												'rgba(255, 207, 207, 1)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(128, 98, 98, 1)',
+												'rgba(0, 0, 0, 1)',
+												'rgba(128, 128, 128, 1)'
+											],
+											fill: false,
+											borderWidth: 1
+										}]
+									},
+									options: {
+										scales: {
+											yAxes: [{
+												ticks: {
+													beginAtZero: true
+												}
+											}]
+										}
+									}
+								});
+							</script>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="ibox">
+					<div class="ibox-body">
+						<div class="flexbox mb-4">
+							<div>
+								<h3 class="m-0">Grafik Yang Sering Pinjam</h3>
+							</div>
+						</div>
+						<div>
+							<?php
+							foreach ($grafik_buku_pinjam as $key => $grafik) {
+								$nama[] = $grafik->nama;
+								$jml_user[] = $grafik->jml_user;
+							}
+							?>
+							<canvas id="myChar" style="height:260px;"></canvas>
+							<script>
+								var ctx = document.getElementById('myChar');
+								var myChart = new Chart(ctx, {
+									type: 'bar',
+									data: {
+										labels: <?= json_encode($nama) ?>,
+										datasets: [{
+											label: 'Grafik Yang Sering Pinjam',
+											data: <?= json_encode($jml_user) ?>,
+											backgroundColor: [
+												'rgba(255, 99, 132, 0.80)',
+												'rgba(54, 162, 235, 0.80)',
+												'rgba(255, 206, 86, 0.80)',
+												'rgba(75, 192, 192, 0.80)',
+												'rgba(153, 102, 255, 0.80)',
+												'rgba(255, 159, 64, 0.80)',
+												'rgba(201, 76, 76, 0.3)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(0, 140, 162, 1)',
+												'rgba(158, 109, 8, 1)',
+												'rgba(201, 76, 76, 0.8)',
+												'rgba(0, 129, 212, 1)',
+												'rgba(201, 77, 201, 1)',
+												'rgba(255, 207, 207, 1)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(128, 98, 98, 1)',
+												'rgba(0, 0, 0, 1)',
+												'rgba(128, 128, 128, 1)',
+												'rgba(255, 99, 132, 0.80)',
+												'rgba(54, 162, 235, 0.80)',
+												'rgba(255, 206, 86, 0.80)',
+												'rgba(75, 192, 192, 0.80)',
+												'rgba(153, 102, 255, 0.80)',
+												'rgba(255, 159, 64, 0.80)',
+												'rgba(201, 76, 76, 0.3)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(0, 140, 162, 1)',
+												'rgba(158, 109, 8, 1)',
+												'rgba(201, 76, 76, 0.8)',
+												'rgba(0, 129, 212, 1)',
+												'rgba(201, 77, 201, 1)',
+												'rgba(255, 207, 207, 1)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(128, 98, 98, 1)',
+												'rgba(0, 0, 0, 1)',
+												'rgba(128, 128, 128, 1)'
+											],
+											borderColor: [
+												'rgba(255, 99, 132, 1)',
+												'rgba(54, 162, 235, 1)',
+												'rgba(255, 206, 86, 1)',
+												'rgba(75, 192, 192, 1)',
+												'rgba(153, 102, 255, 1)',
+												'rgba(255, 159, 64, 1)',
+												'rgba(201, 76, 76, 0.3)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(0, 140, 162, 1)',
+												'rgba(158, 109, 8, 1)',
+												'rgba(201, 76, 76, 0.8)',
+												'rgba(0, 129, 212, 1)',
+												'rgba(201, 77, 201, 1)',
+												'rgba(255, 207, 207, 1)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(128, 98, 98, 1)',
+												'rgba(0, 0, 0, 1)',
+												'rgba(128, 128, 128, 1)',
+												'rgba(255, 99, 132, 1)',
+												'rgba(54, 162, 235, 1)',
+												'rgba(255, 206, 86, 1)',
+												'rgba(75, 192, 192, 1)',
+												'rgba(153, 102, 255, 1)',
+												'rgba(255, 159, 64, 1)',
+												'rgba(201, 76, 76, 0.3)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(0, 140, 162, 1)',
+												'rgba(158, 109, 8, 1)',
+												'rgba(201, 76, 76, 0.8)',
+												'rgba(0, 129, 212, 1)',
+												'rgba(201, 77, 201, 1)',
+												'rgba(255, 207, 207, 1)',
+												'rgba(201, 77, 77, 1)',
+												'rgba(128, 98, 98, 1)',
+												'rgba(0, 0, 0, 1)',
+												'rgba(128, 128, 128, 1)'
+											],
+											fill: false,
+											borderWidth: 1
+										}]
+									},
+									options: {
+										scales: {
+											yAxes: [{
+												ticks: {
+													beginAtZero: true
+												}
+											}]
+										}
+									}
+								});
+							</script>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12">
+				<div class="ibox">
+					<div class="ibox-body">
+						<div class="flexbox mb-4">
+							<div>
+								<h3 class="m-0">Grafik Yang Sering baca</h3>
+							</div>
+						</div>
+						<div>
+							<?php
+							foreach ($grafik_buku_baca as $key => $grafik) {
+								$nm[] = $grafik->nm;
+								$jml_baca[] = $grafik->jml_baca;
+							}
+							?>
+							<canvas id="myCha" style="height:260px;"></canvas>
+							<script>
+								var ctx = document.getElementById('myCha');
+								var myChart = new Chart(ctx, {
+									type: 'bar',
+									data: {
+										labels: <?= json_encode($nm) ?>,
+										datasets: [{
+											label: 'Grafik Yang Sering Baca',
+											data: <?= json_encode($jml_baca) ?>,
 											backgroundColor: [
 												'rgba(255, 99, 132, 0.80)',
 												'rgba(54, 162, 235, 0.80)',
