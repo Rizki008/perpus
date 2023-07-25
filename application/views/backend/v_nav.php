accde4d9e327602049b7d7770b3d26826f7c6282
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-25 15:37:30 +0700

    menambahkan session

--- application/views/backend/v_nav.php
+++ application/views/backend/v_nav.php
@@ -6,11 +6,16 @@
 				<img src="<?= base_url('backend/dist') ?>/assets/img/admin-avatar.png" width="45px" />
 			</div>
 			<div class="admin-info">
-				<div class="font-strong"><?= $this->session->userdata('nama'); ?></div><small><?php if ($this->session->userdata('level_user') == '1') { ?>
+				<div class="font-strong"><?= $this->session->userdata('nama'); ?></div>
+				<small>
+					<?php if ($this->session->userdata('level_user') == '1') { ?>
 						Admin
 					<?php } elseif ($this->session->userdata('level_user') == '2') { ?>
-						Anggota
-					<?php } ?></small>
+						Staff
+					<?php } elseif ($this->session->userdata('level_user') == '3') { ?>
+						Kepala Perpustakaan
+					<?php } ?>
+				</small>
 			</div>
 		</div>
 		<ul class="side-menu metismenu">
@@ -20,43 +25,58 @@
 				</a>
 			</li>
 			<li class="heading">MASTER DATA</li>
-			<li>
-				<a href="<?= base_url('buku') ?>"><i class="sidebar-item-icon fa fa-bookmark"></i>
-					<span class="nav-label">Data Buku</span>
-				</a>
-			</li>
+			<?php if ($this->session->userdata('level_user') === '2') { ?>
+				<li>
+					<a href="<?= base_url('buku') ?>"><i class="sidebar-item-icon fa fa-bookmark"></i>
+						<span class="nav-label">Data Buku</span>
+					</a>
+				</li>
+			<?php } ?>
 			<li>
 				<a href="javascript:;"><i class="sidebar-item-icon fa fa-edit"></i>
 					<span class="nav-label">Data Peminjaman</span><i class="fa fa-angle-left arrow"></i></a>
 				<ul class="nav-2-level collapse">
-					<li>
-						<a href="<?= base_url('master/peminjaman') ?>">Peminjaman Buku</a>
-					</li>
-					<li>
-						<a href="<?= base_url('master/pengembalian') ?>">Pengembalian Buku</a>
-					</li>
-					<li>
-						<a href="<?= base_url('master/denda') ?>">Denda Buku</a>
-					</li>
+					<?php if ($this->session->userdata('level_user') === '1' || $this->session->userdata('level_user') === '3') { ?>
+						<li>
+							<a href="<?= base_url('master/peminjaman') ?>">Peminjaman Buku</a>
+						</li>
+						<li>
+							<a href="<?= base_url('master/pengembalian') ?>">Pengembalian Buku</a>
+						</li>
+					<?php } elseif ($this->session->userdata('level_user') === '2') { ?>
+						<li>
+							<a href="<?= base_url('master/peminjaman') ?>">Peminjaman Buku</a>
+						</li>
+						<li>
+							<a href="<?= base_url('master/pengembalian') ?>">Pengembalian Buku</a>
+						</li>
+						<li>
+							<a href="<?= base_url('master/denda') ?>">Pembayaran Denda Buku</a>
+						</li>
+					<?php } ?>
 				</ul>
 			</li>
-			<li class="heading">SARAN</li>
-			<li>
-				<a href="<?= base_url('master/saran_buku') ?>"><i class="sidebar-item-icon fa fa-smile-o"></i>
-					<span class="nav-label">Saran</span>
-				</a>
-			</li>
-			<li class="heading">LAPORAN</li>
-			<li>
-				<a href="<?= base_url('laporan') ?>"><i class="sidebar-item-icon fa fa-info"></i>
-					<span class="nav-label">Laporan</span>
-				</a>
-			</li>
+			<?php if ($this->session->userdata('level_user') === '3') { ?>
+				<li class="heading">SARAN</li>
+				<li>
+					<a href="<?= base_url('master/saran_buku') ?>"><i class="sidebar-item-icon fa fa-smile-o"></i>
+						<span class="nav-label">Saran</span>
+					</a>
+				</li>
+				<li class="heading">LAPORAN</li>
+				<li>
+					<a href="<?= base_url('laporan') ?>"><i class="sidebar-item-icon fa fa-info"></i>
+						<span class="nav-label">Laporan</span>
+					</a>
+				</li>
+			<?php } ?>
 			<li class="heading">USER</li>
 			<li>
-				<a href="<?= base_url('admin/user') ?>"><i class="sidebar-item-icon fa fa-user-circle-o"></i>
-					<span class="nav-label">User</span>
-				</a>
+				<?php if ($this->session->userdata('level_user') === '1') { ?>
+					<a href="<?= base_url('admin/user') ?>"><i class="sidebar-item-icon fa fa-user-circle-o"></i>
+						<span class="nav-label">User</span>
+					</a>
+				<?php } ?>
 				<a href="<?= base_url('admin/anggota') ?>"><i class="sidebar-item-icon fa fa-user-plus"></i>
 					<span class="nav-label">Data Anggota</span>
 				</a>
